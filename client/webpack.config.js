44093255dba0dce20188505bd6259bbd299b0b3f
Kush Patel <kushrpatel06@yahoo.com>
2023-01-25 10:40:39 -0500

    added logic to install.js and webpack.config.js completed installation feature logic added and configured workbox plugins for service work and manifest file added css loader and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Text Editor"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js"
+      }),
+      new WebpackPwaManifest({
+        inject: true,
+        fingerprints: false,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Offline compatible PWA Text Editor",
+        background_color: "#407ad6",
+        theme_color: "#407ad6",
+        start_ur: "/",
+        publicPath: "/",
+        icons: [{
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        }
+        ]
+      })
     ],
 
     module: {
-      rules: [
+      rules: [{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        //dont target node module js files by mistake
+        excludes: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          option: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+          }
+        }
+      }
         
       ],
     },
